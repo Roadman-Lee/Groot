7c4a07c1aa610ffac66d4b40612df4f00113de92
Roadman <rdmshine@Roadmanui-iMac.local>
2022-05-04 13:07:53 +0900

    add two test case

--- tabom/tests/test_like_service.py
+++ tabom/tests/test_like_service.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from tabom.models.article import Article
 from tabom.models.user import User
 from tabom.services.like_service import do_like
+from django.db import IntegrityError
 
 
 class TestLikeService(TestCase):
@@ -26,6 +27,23 @@ class TestLikeService(TestCase):
 
         # Expect
         do_like(user.id, article.id)
-        from django.db import IntegrityError
         with self.assertRaises(IntegrityError):
             do_like(user.id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_user_does_not_exist(self) -> None:
+        # Given
+        invalid_user_id = 9988
+        article = Article.objects.create(title="test_title")
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(invalid_user_id, article.id)
+
+    def test_it_should_raise_exception_when_like_an_article_does_not_exist(self) -> None:
+        # Given
+        user = User.objects.create(name="test")
+        invalid_article_id = 9988
+
+        # Expect
+        with self.assertRaises(IntegrityError):
+            do_like(user.id, invalid_article_id)
